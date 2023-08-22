0bd971bfc70105c9d9ec1a96308a3198771157e3
JUAN jose <jnina@frba.utn.edu.ar>
2023-08-22 12:39:45 -0300

    agregar event listener y limpiar

--- clase10/src/components/BtnCounts.jsx
+++
@@ -1,9 +0,0 @@
-function BtnCounts({ onConfirm }) {
-  const handleClick = () => {
-    onConfirm();
-  };
-
-  return <button onClick={handleClick}>sumar al carrito</button>;
-}
-
-export default BtnCounts;
